a456bf5df61ed923782c9d66dd7d6c8ce474968d
Abdelrahman saeed <117317455+abdelrahmansaeed19@users.noreply.github.com>
2025-05-18 18:23:46 +0300

    Update joins_queries.sql

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,6 +3,6 @@ USE AirBnB_Clone
 
 SELECT * FROM Booking INNER JOIN users ON Booking.user_id = users.user_id
 
-SELECT * FROM property LEFT JOIN Review ON property.property_id = Review.property_id
+SELECT * FROM property LEFT JOIN Review ON property.property_id = Review.property_id ORDER BY property.property_id
 
 SELECT * FROM users FULL OUTER JOIN Booking ON users.user_id = Booking.user_id
