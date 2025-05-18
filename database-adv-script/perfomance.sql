88ff3299d98f6bf87f1a651b57dd431c96a55cae
Abdelrahman saeed <117317455+abdelrahmansaeed19@users.noreply.github.com>
2025-05-18 19:17:34 +0300

    Update perfomance.sql

--- database-adv-script/perfomance.sql
+++ database-adv-script/perfomance.sql
@@ -4,6 +4,29 @@ SELECT
     b.end_date,
     b.total_price,
     b.status,
+    u.user_id      AS booking_user_id,
+    u.first_name,
+    u.last_name,
+    p.property_id  AS booked_property_id,
+    p.name         AS property_name,
+    p.location     AS property_location,
+    pay.payment_id,
+    pay.amount     AS payment_amount,
+    pay.payment_date
+FROM Booking AS b,
+     users   AS u,
+     property AS p,
+     Payment  AS pay
+WHERE b.user_id = u.user_id
+  AND b.property_id = p.property_id
+  AND pay.booking_id = b.booking_id;
+
+SELECT
+    b.booking_id,
+    b.start_date,
+    b.end_date,
+    b.total_price,
+    b.status,
     u.user_id,
     u.first_name,
     u.last_name,
